010860380a29bc3f73a3dfc1d8290a33b7699977
Eddie <ehsu0407@gmail.com>
2020-12-17 22:03:47 -0500

    Enable cors on RPC endpoints

--- odoo/addons/base/controllers/rpc.py
+++ odoo/addons/base/controllers/rpc.py
@@ -49,7 +49,7 @@ class RPC(Controller):
         result = dispatch_rpc(service, method, params)
         return dumps((result,), methodresponse=1, allow_none=False)
 
-    @route("/xmlrpc/<service>", auth="none", methods=["POST"], csrf=False, save_session=False)
+    @route("/xmlrpc/<service>", auth="none", methods=["OPTIONS", "POST"], csrf=False, save_session=False, cors='*')
     def xmlrpc_1(self, service):
         """XML-RPC service that returns faultCode as strings.
 
@@ -62,7 +62,7 @@ class RPC(Controller):
             response = wsgi_server.xmlrpc_handle_exception_string(error)
         return Response(response=response, mimetype='text/xml')
 
-    @route("/xmlrpc/2/<service>", auth="none", methods=["POST"], csrf=False, save_session=False)
+    @route("/xmlrpc/2/<service>", auth="none", methods=["OPTIONS", "POST"], csrf=False, save_session=False, cors='*')
     def xmlrpc_2(self, service):
         """XML-RPC service that returns faultCode as int."""
         try:
@@ -71,7 +71,7 @@ class RPC(Controller):
             response = wsgi_server.xmlrpc_handle_exception_int(error)
         return Response(response=response, mimetype='text/xml')
 
-    @route('/jsonrpc', type='json', auth="none", save_session=False)
+    @route('/jsonrpc', type='json', auth="none", save_session=False, cors='*')
     def jsonrpc(self, service, method, args):
         """ Method used by client APIs to contact OpenERP. """
         return dispatch_rpc(service, method, args)
